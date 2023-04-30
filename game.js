3715f9b6a188ab0b0258a487a66f233b0e2e11a9
A-Fierce <a.n.fierce@gmail.com>
2023-04-30 21:08:25 +0600

    First commit

---
+++ game.js
@@ -0,0 +1,20 @@
+export default class Game {};
+export class GameSavingData {};
+export function readGameSaving();
+export function writeGameSaving();
+import Character from "./domain";
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
